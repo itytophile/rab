d3692c3eb60ca1d6a02fb4be5d43f72061417bc2
ityt <imran.tierce@protonmail.com>
2021-04-22 01:19:59 +0200

    default font changed

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod ui_iced;
 
 use iced::{Sandbox, Settings};
 
+const FONT: &[u8] = include_bytes!("fonts/FiraSans-Regular.ttf");
+
 pub fn main() -> iced::Result {
-    ui_iced::MainApp::run(Settings::default())
+    ui_iced::MainApp::run(Settings {default_font: Some(FONT),..Default::default()})
 }
